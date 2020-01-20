cdc115606410a007ed7f9dcb787560da485bbc67
liber.roi <liber.roi@gmail.com>
2020-01-20 17:13:40 +0200

    Updates

---
+++ precache-manifest.9315366281efe07012a012e387ccf30d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ef21514eb7823dc6b510a93582163d",
+    "url": "/Craft-and-Mix/index.html"
+  },
+  {
+    "revision": "6dc5dd6495421f3fcddb",
+    "url": "/Craft-and-Mix/static/css/main.608e6118.chunk.css"
+  },
+  {
+    "revision": "4783d6a4d6c56a3b079c",
+    "url": "/Craft-and-Mix/static/js/2.1ffb93d0.chunk.js"
+  },
+  {
+    "revision": "6dc5dd6495421f3fcddb",
+    "url": "/Craft-and-Mix/static/js/main.038bed83.chunk.js"
+  },
+  {
+    "revision": "312f5b207e244c39420d",
+    "url": "/Craft-and-Mix/static/js/runtime-main.f0a0a9d5.js"
+  },
+  {
+    "revision": "c718425a424ac1947f840143daa231d6",
+    "url": "/Craft-and-Mix/static/media/About us.c718425a.jpeg"
+  },
+  {
+    "revision": "5234f9444bbb6eac6bfed8bd925157c7",
+    "url": "/Craft-and-Mix/static/media/Mode.5234f944.jpeg"
+  },
+  {
+    "revision": "5fd669cc203bda5bd9acd73be3e17c8d",
+    "url": "/Craft-and-Mix/static/media/On the Menu.5fd669cc.jpeg"
+  },
+  {
+    "revision": "83a59f4bc4d17a7f7a6bc1ee333040a2",
+    "url": "/Craft-and-Mix/static/media/Our Services.83a59f4b.jpeg"
+  },
+  {
+    "revision": "0ebaf998301c1e9b4a82941c0bd68f57",
+    "url": "/Craft-and-Mix/static/media/gold.0ebaf998.PNG"
+  }
+]);
