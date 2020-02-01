9cc107e95eb33f0b7c3e5bdc66236b6e1cfd13eb
liber.roi <liber.roi@gmail.com>
2020-02-01 06:19:23 +0200

    parallax test

--- src/containers/TopLayout/TopLayout.js
+++ src/containers/TopLayout/TopLayout.js
@@ -15,53 +15,57 @@ import './style.scss';
 
 class TopLayout extends Component {
 
-    parallax = (photo, num) => {
-        return <ParallaxProvider>
-            <Parallax className="parallax" y={[-30, 30]} tagOuter="figure">
-                <img src={photo} alt={'backPhoto'}
-                        className={num === 1 ? 'backPhoto backPhoto1' : 'backPhoto backPhoto2'}/>
-            </Parallax>
-        </ParallaxProvider>
+    parallax = photo => {
+        return <Parallax className="parallax" y={[-30, 30]} tagOuter="figure">
+            <img src={photo} alt={'backPhoto'}/>
+        </Parallax>
     };
 
     render() {
-        const { windowHeight, windowScrollY, centerPhotoItem, setPhoto, clients, topCarousel } = this.props;
+        const { windowHeight, windowScrollY, centerPhotoItem, setPhoto, clients, topCarousel, small } = this.props;
         let height10vh = windowHeight / 10;
         const carouselExit = windowScrollY > (windowHeight - height10vh * 2.5);
 
         return (
-            <div className="top_layout">
-                {!carouselExit && <TopCarousel topCarousel={topCarousel}/>}
-                <OurServices/>
-                <div className={'photo'}>
-                    {this.parallax(backPhoto1, 1)}
-                </div>
-                <Mood/>
-                <div className={'clear'}/>
-                <Clients clients={clients}/>
-                <div className={'photo'}>
-                    {this.parallax(backPhoto2, 2)}
-                </div>
-                {centerPhotoItem && <Slide bottom duretion={500}>
-                    <div className={'pop_up_wrapper'} onClick={() => setPhoto(false)}>
-                        <PopUpImg photoObj={centerPhotoItem}/>
+            <ParallaxProvider>
+                <div className="top_layout">
+                    {!carouselExit
+                        ? <Parallax className="parallax_carousel" y={[-30, 30]} tagOuter="figure">
+                            <TopCarousel topCarousel={topCarousel}/>
+                        </Parallax>
+                        : <div className={'white_cover'}/>}
+                    <OurServices/>
+                    <div className={'photo'}>
+                        {this.parallax(backPhoto1)}
                     </div>
-                </Slide>}
-                <Footer/>
-            </div>
+                    <Mood/>
+                    <div className={'clear'}/>
+                    <Clients clients={clients}/>
+                    <div className={'photo'}>
+                        {this.parallax(backPhoto2)}
+                    </div>
+                    {centerPhotoItem && <Slide bottom duretion={500}>
+                        <div className={'pop_up_wrapper'} onClick={() => setPhoto(false)}>
+                            <PopUpImg photoObj={centerPhotoItem}/>
+                        </div>
+                    </Slide>}
+                    <Footer/>
+                </div>
+            </ParallaxProvider>
         )
     };
 }
 
 const mapStateToProps = state => {
-    const { windowScrollY, windowHeight, centerPhotoItem, topCarousel, clients } = state.mainReducer;
+    const { windowScrollY, windowHeight, centerPhotoItem, topCarousel, clients, small } = state.mainReducer;
 
     return {
         windowScrollY,
         windowHeight,
         centerPhotoItem,
         topCarousel,
-        clients
+        clients,
+        small
     };
 };
 
