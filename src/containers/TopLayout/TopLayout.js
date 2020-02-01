c322790e9f778265d279ebe4c5522afa6f1d64ab
liber.roi <liber.roi@gmail.com>
2020-02-01 05:21:59 +0200

    parallax

--- src/containers/TopLayout/TopLayout.js
+++ src/containers/TopLayout/TopLayout.js
@@ -6,13 +6,23 @@ import Clients from "../../sections/Clients";
 import TopCarousel from "../../sections/TopCarousel/TopCarousel";
 import { setPhoto } from '../../store/actions/mainActions';
 import Mood from "../../sections/Mood/Mood";
-import backPhoto1 from '../../assets/img/background/crop1.jpg';
-import backPhoto2 from '../../assets/img/background/crop2.jpg';
+import backPhoto1 from '../../assets/img/background/backPhoto1.jpeg';
+import backPhoto2 from '../../assets/img/background/backPhoto2.jpeg';
 import PopUpImg from "../../components/PopUpPhoto";
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
 import { Slide } from "react-reveal";
 import './style.scss';
 
 class TopLayout extends Component {
+
+    parallax = (photo, num) => {
+        return <ParallaxProvider>
+            <Parallax className="parallax" y={[-30, 30]} tagOuter="figure">
+                <img src={photo} alt={'backPhoto'}
+                        className={num === 1 ? 'backPhoto backPhoto1' : 'backPhoto backPhoto2'}/>
+            </Parallax>
+        </ParallaxProvider>
+    };
 
     render() {
         const { windowHeight, windowScrollY, centerPhotoItem, setPhoto, clients, topCarousel } = this.props;
@@ -24,13 +34,13 @@ class TopLayout extends Component {
                 {!carouselExit && <TopCarousel topCarousel={topCarousel}/>}
                 <OurServices/>
                 <div className={'photo'}>
-                    <img src={backPhoto1} alt={'backPhoto'} className={'backPhoto1'}/>
+                    {this.parallax(backPhoto1, 1)}
                 </div>
                 <Mood/>
                 <div className={'clear'}/>
                 <Clients clients={clients}/>
                 <div className={'photo'}>
-                    <img src={backPhoto2} alt={'backPhoto'} className={'backPhoto2'}/>
+                    {this.parallax(backPhoto2, 2)}
                 </div>
                 {centerPhotoItem && <Slide bottom duretion={500}>
                     <div className={'pop_up_wrapper'} onClick={() => setPhoto(false)}>
