dd3a7c9379badcccc1be40aa368c0e0e89ea37e8
liber.roi <liber.roi@gmail.com>
2020-02-03 04:26:17 +0200

    new enteer page logo

--- src/containers/EnterPage/EnterPage.js
+++ src/containers/EnterPage/EnterPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logoWhite from '../../assets/icon/Crift&Mix_W.svg';
 import logo from '../../assets/icon/Crift & Mix_logo icon.svg';
+import Pulse from 'react-reveal/Pulse';
 import './style.scss';
 
 export default function EnterPage() {
@@ -8,16 +9,18 @@ export default function EnterPage() {
     return (
         <div className="enter_page_wrapper">
             <div className={'enter_page'}>
-                <div className={'enter_page_logo_img'}>
-                    <img src={logoWhite} alt={'logo'}/>
-                </div>
-                <div className={'enter_page_spin_logo'}>
-                    <div className="logo_box">
-                        <img src={logo} alt={'logo'} className={'hourglass'}/>
-                    </div>
+                <div className={'pulse_logo'}>
+                    <Pulse forever>
+                        <img src={logoWhite} alt={'logo'}/>
+                    </Pulse>
                 </div>
                 <p>Cocktail bar service</p>
-                <p>and much more</p>
+                <span>and much more</span>
+                <span className={'spin_wrapper'}>
+                    <div className="spin_box">
+                        <img src={logo} alt={'logo'} className={'spin_logo'}/>
+                    </div>
+                </span>
             </div>
         </div>
     );
